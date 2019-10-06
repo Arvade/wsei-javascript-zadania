4cc7566d7029f15ce42040c158a50b21a15cd401
Sebastian Machczyński <arevo989@gmail.com>
2019-10-06 22:01:37 +0200

    Solution for 3_Tablice_wielowymiarowe [Zadanie1]

--- 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
+++ 1_Zadania/obowiązkowe/3_Tablice_wielowymiarowe/js/app.js
@@ -32,3 +32,10 @@ var task2Array = [
     [5, 6, 7, 8],
     [9, 10, 11, 12]
 ];
+
+/* Zadanie 1 */
+function zadanie1() {
+    console.log(task1Array[3][2]);
+    console.log(task1Array[1].length);
+    console.log(task1Array[4][2]);
+}
