6f53fa76df1ef2212e281679559b860ee21be5f8
Arvade <arevo989@gmail.com>
2019-10-06 17:06:45 +0200

    Solution for 2_Tablica [Zadanie1]

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -1,3 +1,4 @@
+/* Zadanie 0 */
 function distFromAvarage(tab) {
     const sum = tab.reduce((a, b) => a + b, 0);
     const average = sum / tab.length;
@@ -5,3 +6,15 @@ function distFromAvarage(tab) {
 }
 
 
+/* Zadanie 1 */
+
+function zadanie1() {
+    const favoriteFruits = ["banana", "apple", "orange"];
+
+    console.log(favoriteFruits[0]);  // First fruit
+    console.log(favoriteFruits[favoriteFruits.length - 1]); // Last fruit
+
+    for (let i = 0; i < favoriteFruits.length; i++) {
+        console.log(favoriteFruits[i]);
+    }
+}
