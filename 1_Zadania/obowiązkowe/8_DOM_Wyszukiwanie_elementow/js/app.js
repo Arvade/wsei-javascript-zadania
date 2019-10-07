4a25878d28b0e36765c13baa405704d7eccfca6a
Sebastian Machczyński <arevo989@gmail.com>
2019-10-07 22:28:09 +0200

    Solution for 8_DOM_Wyszukiwanie_elementow [Zadanie1]

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -3,8 +3,20 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania. Odpowiedzi oddzielaj komentarzami.
      */
 
+    /* Zadanie 0 */
     const element = document.getElementsByClassName("title")[0];
     getDataAnimation(element);
+
+
+    /* Zadanie 1.1 */
+    const elHome1 = document.getElementById('home');
+    const elHome2 = document.querySelector('#home');
+
+    /* Zadanie 1.2 */
+    const firstLiElement = document.querySelector('li:first-child[data-direction]');
+
+    /* Zadanie 1.3 */
+    const firstElementWithClass = document.querySelector("div.block:first-child");
 });
 
 function getDataAnimation(el) {
