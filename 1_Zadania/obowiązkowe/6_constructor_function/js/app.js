b7df7c6854970398d64fe7abb15a4bd6340a2fd8
Sebastian Machczyński <arevo989@gmail.com>
2019-10-07 19:24:05 +0200

    Solution for 6_constructor_function [Zadanie0]

---
+++ 1_Zadania/obowiązkowe/6_constructor_function/js/app.js
@@ -0,0 +1,19 @@
+const Basket = function () {
+    this.products = [];
+    this.sum = 0;
+
+    this.addProduct = function (name, price) {
+        this.products.push({name: name, price: price});
+        this.sum += price;
+    };
+
+    this.showBasket = function () {
+        for (let i = 0; i < this.products.length; i++) {
+            const product = this.products[i];
+
+            console.log(`${product.name}  ${product.price}`);
+        }
+        console.log(`Total cost:  ${this.sum}`)
+    }
+};
+
