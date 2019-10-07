adb9c25d5f25ead917cfa3a17a5363415fb88557
Sebastian Machczyński <arevo989@gmail.com>
2019-10-07 22:53:38 +0200

    Solution for 8_DOM_Wyszukiwanie_elementow [Zadanie3]

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -17,6 +17,28 @@ document.addEventListener("DOMContentLoaded", function () {
 
     /* Zadanie 1.3 */
     const firstElementWithClass = document.querySelector("div.block:first-child");
+
+    /* Zadanie 2.1 */
+    const allListItemsInNav = document.querySelectorAll('nav li');
+    console.log('allListItemsInNav : ', allListItemsInNav.length);
+
+    /* Zadanie 2.2 */
+    const paragrafsInsideDiv = document.querySelectorAll('div p');
+    console.log('paragrafsInsideDiv : ', paragrafsInsideDiv.length);
+
+    /* Zadanie 2.3 */
+    const divsInsideArticle = document.querySelectorAll('article div');
+    console.log('divsInsideArticle : ', divsInsideArticle.length);
+
+    /* Zadanie 3.1 */
+    const articlesWithClassFirst = document.querySelectorAll('article[class=first]');
+    console.log('articlesWithClassFirst : ', articlesWithClassFirst);
+
+    /* Zadanie 3.2 */
+    for (let i = 0; i < articlesWithClassFirst.length; i++) {
+        const h1Elements = articlesWithClassFirst[i].getElementsByTagName("h1");
+        console.log(`amount of [h1Elements] for [articleWithClassFirst #${i}] : ${h1Elements.length}`)
+    }
 });
 
 function getDataAnimation(el) {
