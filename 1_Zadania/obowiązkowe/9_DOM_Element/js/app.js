c827bd08103f164e8ecc15ce5b97c01e0e578d86
Sebastian Machczyński <arevo989@gmail.com>
2019-10-07 23:51:21 +0200

    All Solutions for 9_DOM_Element

--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -9,4 +9,84 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania
      */
 
+    function getDatasInfo(elements) {
+        const result = [];
+
+        for (let i = 0; i < elements.length; i++) {
+            result.push(elements[i].getAttribute('data-color'));
+        }
+
+        return result;
+    }
+
+    /* Zadanie 1 */
+    console.log('homeElement : ', homeElement);
+    console.log('childElements : ', childElements);
+    for (let i = 0; i < childElements.length; i++) {
+        const el = childElements[i];
+        console.log(`\t${el.localName} : ${el.className}`)
+    }
+
+    console.log('banner : ', banner);
+    console.log('blocks : ', blocks);
+    for (let i = 0; i < blocks.length; i++) {
+        const el = blocks[i];
+        console.log(`\t${el.localName} : ${el.className}`);
+    }
+
+    console.log('links : ', links);
+    for (let i = 0; i < links.length; i++) {
+        const el = links[i];
+        console.log(`\t${el.localName} : ${el.className}`);
+    }
+
+    /* Zadanie 2 */
+    for (let i = 0; i < blocks.length; i++) {
+        const block = blocks[i];
+        /*
+            innerHTML - zwraca 'zawartość' elementu
+            outerHTML - zwraca 'zawartość' wraz z samym elementem który czytamy
+        */
+        console.log('innerHTML : ', block.innerHTML);
+        console.log('outerHTML : ', block.outerHTML);
+
+        block.innerHTML = "Nowa wartość diva o klasie blocks";
+    }
+
+    /* Zadanie 3 */
+    const mainFooterEl = document.getElementById('mainFooter');
+
+    function getId(element) {
+        return element.id;
+    }
+
+    /* Zadanie 4 */
+    function getTags(elements) {
+        const result = [];
+        for (let i = 0; i < elements.length; i++) {
+            result.push(elements[i].localName);
+        }
+        return result;
+    }
+
+    /* Zadanie 5 */
+    function getClassInfo(element) {
+        return element.className.split(' ');
+    }
+
+    console.log(getClassInfo(banner));
+
+    /* Zadanie 6 */
+    const listItemsInsideNav = document.querySelectorAll("nav li");
+
+    function setDataDirection(elements) {
+        elements.forEach(el => {
+            const dataAttr = el.getAttribute('data-direction');
+            if (dataAttr == null) {
+                el.setAttribute('data-direction', 'top');
+            }
+        });
+    }
+
+    setDataDirection(listItemsInsideNav);
 });
