0fe947f7aec31994f9c72ef5f2d681636a0bb535
Sebastian Machczyński <arevo989@gmail.com>
2019-10-09 20:26:18 +0200

    Solution for 10_DOM_Wiecej_o_elemencie [Zadanie1]

--- 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
+++ 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania
      */
 
+    /* Zadanie 0 */
     const listItems = document.querySelectorAll(".ex5 li");
     let greenBackgroundStep = 1;
 
@@ -14,4 +15,26 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     listItems[4].classList.add("big");
+
+    /* Zadanie 1 */
+    const chromeImg = document.querySelector(".chrome");
+    const edgeImg = document.querySelector(".edge");
+    const firefoxImg = document.querySelector(".firefox");
+
+    const chromeLink = document.querySelector(".chrome + a");
+    const edgeLink = document.querySelector(".edge + a");
+    const firefoxLink = document.querySelector(".firefox + a");
+
+    chromeImg.style.backgroundImage = "url('assets/img/chrome.png')";
+    chromeImg.style.width = "100px";
+
+    edgeImg.style.backgroundImage = "url('assets/img/edge.png')";
+    firefoxImg.style.backgroundImage = "url('assets/img/firefox.png')";
+
+    chromeLink.innerHTML = "Chrome";
+    firefoxLink.innerHTML = "Firefox";
+
+    chromeLink.setAttribute("href", "https://www.google.com/intl/pl_pl/chrome/");
+    edgeLink.setAttribute("href", "https://www.microsoft.com/pl-pl/windows/microsoft-edge");
+    firefoxLink.setAttribute("href", "https://www.mozilla.org/pl/firefox/")
 });
