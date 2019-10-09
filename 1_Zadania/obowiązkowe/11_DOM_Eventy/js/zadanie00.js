0178c0b97f69d7b2d4e734e9fb3fd551d054b3a1
Sebastian Machczyński <arevo989@gmail.com>
2019-10-09 21:33:56 +0200

    Solution for 11_DOM_Eventy [Zadanie0]

--- 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie00.js
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie00.js
@@ -7,10 +7,15 @@ document.querySelectorAll(".parent")
             }
         });
 
-        el.addEventListener("mouseleave", (event) => {
-            const content = event.target.children;
-            for (let i = 0; i < content.length; i++) {
-                content[i].style.display = "none";
+        el.addEventListener("mouseleave", () => hideChildrenElement());
+
+        function hideChildrenElement() {
+            const parents = this.document.querySelectorAll(".parent");
+            for (let i = 0; i < parents.length; i++) {
+                const parent = parents[i];
+                if (parent === el) {
+                    parent.children[0].style.display = "none";
+                }
             }
-        })
+        }
     });
