09de136bf97c3e35add300b3cc0d825b7feebf67
Sebastian Machczyński <arevo989@gmail.com>
2019-10-09 19:30:07 +0200

    Solution for 10_DOM_Wiecej_o_elemencie [Zadanie0]

--- 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
+++ 1_Zadania/obowiązkowe/10_DOM_Wiecej_o_elemencie/js/app.js
@@ -3,4 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania
      */
 
+    const listItems = document.querySelectorAll(".ex5 li");
+    let greenBackgroundStep = 1;
+
+    for (let i = 0; i < listItems.length; i++) {
+        if (greenBackgroundStep <= listItems.length - 1) {
+            listItems[greenBackgroundStep].style.backgroundColor = "green";
+            greenBackgroundStep += 2;
+        }
+    }
+
+    listItems[4].classList.add("big");
 });
