55da898534fbdb2b044b9e4c1b74e96429bb97bd
Sebastian Machczyński <arevo989@gmail.com>
2019-10-12 20:53:55 +0200

    [Dodatkowe] Solution for 5_Tooltips

--- 1_Zadania/dodatkowe/5_Tooltips/js/app.js
+++ 1_Zadania/dodatkowe/5_Tooltips/js/app.js
@@ -1,3 +1,21 @@
 /*
  <span class="tooltipText">Text tooltipa</span>
  */
+document.addEventListener("DOMContentLoaded", () => {
+    const tooltips = [...document.querySelectorAll(".tooltip")];
+    const tooltipElement = document.createElement("span");
+    tooltipElement.classList.add("tooltipText");
+
+    tooltips.forEach(tooltip => {
+        const tip = tooltipElement.cloneNode(false);
+
+        tooltip.addEventListener("mouseover", function () {
+            tip.innerText = this.getAttribute("data-text");
+            this.appendChild(tip);
+        });
+
+        tooltip.addEventListener("mouseout", function () {
+            this.removeChild(tip);
+        });
+    })
+});
