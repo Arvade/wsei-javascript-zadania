3e671177070a3102d7878473595e2896c66e55cc
Sebastian Machczyński <arevo989@gmail.com>
2019-10-12 23:16:04 +0200

    [Dodatkowe] Solution for 7 [Zadanie2]

--- 1_Zadania/dodatkowe/7/js/app.js
+++ 1_Zadania/dodatkowe/7/js/app.js
@@ -3,3 +3,15 @@ function getNumber(n, tab) {
     return tab.includes(n);
 }
 
+/* Zadanie 2 */
+function createIdentityMatrix(rows, columns) {
+    const result = [];
+    for (let row = 0; row < rows; row++) {
+        result[row] = [];
+        for (let col = 0; col < columns; col++) {
+            const value = col === row;
+            result[row][col] = +value;
+        }
+    }
+    return result;
+}
