907be666931b558cc20b2d4e8aa69cd072b14bfb
Sebastian Machczyński <arevo989@gmail.com>
2019-10-13 11:50:57 +0200

    [Dodatkowe] Solution for 12_let_const [Zadanie 4]

--- 1_Zadania/dodatkowe/12_let_const/js/zadanie04.js
+++ 1_Zadania/dodatkowe/12_let_const/js/zadanie04.js
@@ -2,12 +2,24 @@
 //TODO: spróbuj zmienić zmienną name nadpisująć ją niżej innym imieniem.
 const name = "Agata";
 
+/*
+    name = "changed";
+    TypeError: Assignment to constant variable
+
+    Ponieważ:   'const' to stała
+ */
 
 //TODO:
 //a) spróbuj zmienić zmienną simpleArray nadpisująć ją niżej inną tablicą.
 //b) spróbuj dodać nową wartość do tablicy
 //c) spróbuj zmienić którąkolwiek wartość z tablicy simpleArray
 const simpleArray = [1, 2, 3, 4];
+
+/*
+    a) Nie można - 'const' to stała
+    b) Możemy modyfikować - ponieważ nie zmieniamy wartości 'simpleArray'
+    c) Możemy zmienić - ponieważ nie zmieniamy wartości 'simpleArray'
+ */
 
 
 //TODO:
@@ -17,4 +29,13 @@ const simpleArray = [1, 2, 3, 4];
 const dog = {
     name: "Puszek",
     skill: "killing"
-}
+};
+
+/*
+    a) Możemy
+    b) Możemy
+    c) Możemy
+
+    Ponieważ: 'const' chroni przed nadpisaniem referencji do elementu (np. dog = {})
+
+ */
