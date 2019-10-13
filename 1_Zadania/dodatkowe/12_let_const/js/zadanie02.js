da3a48fc0e4754be4d3bb4f094d21179bd500db1
Sebastian Machczyński <arevo989@gmail.com>
2019-10-13 11:39:54 +0200

    [Dodatkowe] Solution for 12_let_const [Zadanie 2]

--- 1_Zadania/dodatkowe/12_let_const/js/zadanie02.js
+++ 1_Zadania/dodatkowe/12_let_const/js/zadanie02.js
@@ -7,7 +7,27 @@ const lover = 'Shrek';
     let character = "ogrzyca";
     console.log("Teraz jest: " + timeOfDay + " i Fiona to: " + character + ", a jej ukochany to: " + lover);
     // TODO: Napisz, co kryje się pod zmiennymi i wyjaśnij dlaczego.
+    /*
+        timeOfDay - noc
+        character - ogrzyca
+        lover - Shrek
+
+        Użyte tutaj rodzaje zmiennych, 'const' oraz 'let' mają zasięg blokowy, tak więc
+        zmienne jako pierwsze będą odczytane z bloku w którym jesteśmy. Oczywiście aktualny blok
+        ma dostęp również do zmiennych z zewnątrz (outer scope).
+
+     */
 }
 
 console.log("Teraz jest: " + timeOfDay + " i Fiona to: " + character + ", a jej ukochany to: " + lover);
 // TODO: Napisz, co kryje się pod zmiennymi i wyjaśnij dlaczego.
+
+/*
+    timeOfDay - dzień
+    character - księżniczka
+    lover - Shrek
+
+    Zmiany które wydawało by się, że zaszły w powyższym bloku nie mają tak naprawdę wpływu na zmienne na 'zewnątrz'.
+    W ramach powyższego bloku (scope'a) zostały utworzone 'nowe' zmienne i są dostępne tylko w ramach niego.
+
+*/
